3c85fc07ccaecc4e2adb02dedac922eaa9d5e7d7
Slava Chalnev <slava.chalnev@gmail.com>
2023-08-25 14:13:50 +1000

    cyan

--- token_print/colourful.py
+++ token_print/colourful.py
@@ -16,7 +16,7 @@ def print_colored_tokens(text: str, tokenizer):
     input_ids = tokenizer.encode(text, add_special_tokens=False)
     tokens = [tokenizer.decode([id], clean_up_tokenization_spaces=False) for id in input_ids]
 
-    colors = ['on_magenta', 'on_green', 'on_yellow', 'on_red', 'on_blue']
+    colors = ['on_magenta', 'on_green', 'on_yellow', 'on_red', 'on_cyan']
     color_iter = cycle(colors)
 
     # Iterate through tokens and print them in corresponding colors
